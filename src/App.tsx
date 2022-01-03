89e86954e4df22bc1bd557d2ed63c3eabf69a8bc
nekoya <nekoya@voyagegroup.com>
2022-01-03 19:17:54 +0900

    Apply base design

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,38 @@
+import { Box, createTheme, Paper, ThemeProvider } from "@mui/material";
+import { blue } from "@mui/material/colors";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#0f546b",
+            light: "#ecf0f1",
+        },
+        secondary: {
+            main: blue[50],
+        },
+    },
+});
+
 export const App: React.VFC = () => {
-    return <div>hello</div>;
+    return (
+        <ThemeProvider theme={theme}>
+            <Box
+                sx={{
+                    minHeight: "100vh",
+                    display: "flex",
+                    flexDirection: "column",
+                }}
+            >
+                <Header />
+                <Box mx={2} my={10}>
+                    <Paper>
+                        <Box p={2}>fooo</Box>
+                    </Paper>
+                </Box>
+                <Footer />
+            </Box>
+        </ThemeProvider>
+    );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { Box, Link } from "@mui/material";
+import { IconText } from "./IconText";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { FlexBox } from "./FlexBox";
+import ArchiveIcon from "@mui/icons-material/Archive";
+
+export const Footer: React.VFC = () => {
+    return (
+        <Box
+            sx={{
+                flexGrow: 1,
+                bgcolor: "primary.light",
+                p: 2,
+            }}
+        >
+            <FlexBox gap={4} justifyContent="center">
+                <Link href="https://twitter.com/nekoya">
+                    <IconText icon={TwitterIcon}>nekoya</IconText>
+                </Link>
+                <Link href="https://github.com/nekoya">
+                    <IconText icon={GitHubIcon}>nekoya</IconText>
+                </Link>
+                <Link href="https://nekoya.github.io/blog/archives/">
+                    <IconText icon={ArchiveIcon}>past entries</IconText>
+                </Link>
+            </FlexBox>
+        </Box>
+    );
+};
