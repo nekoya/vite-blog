6ec7ddf919eeb4f7396c9983bf732e2cd4e1249b
nekoya <nekoya@voyagegroup.com>
2022-01-05 00:08:46 +0900

    Show posts dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import {
     Box,
     createTheme,
-    Divider,
     Link,
     Paper,
     ThemeProvider,
     Typography,
 } from "@mui/material";
 import { blue } from "@mui/material/colors";
-import { useState } from "react";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
-import Example, { meta } from "./example.mdx";
 
 const theme = createTheme({
     palette: {
@@ -25,8 +22,22 @@ const theme = createTheme({
     },
 });
 
+interface PostMeta {
+    path: string;
+    title: string;
+    published: string;
+}
+
+const modules = import.meta.globEager("./posts/*.mdx");
+const posts: PostMeta[] = [];
+for (const path in modules) {
+    const match = path.match(/\.\/posts\/(.*)\.mdx$/);
+    if (match !== null) {
+        posts.push({ ...modules[path].meta, path: match[1] });
+    }
+}
+
 export const App: React.VFC = () => {
-    const [open, setOpen] = useState(false);
     return (
         <ThemeProvider theme={theme}>
             <Box
@@ -37,29 +48,25 @@ export const App: React.VFC = () => {
                 }}
             >
                 <Header />
-                <Box m={2} my={4}>
-                    <Paper sx={{ px: 2, py: 0.5 }}>
-                        <Typography variant="body2">
-                            {meta.published}
-                        </Typography>
-                        <Typography variant="h6">{meta.title}</Typography>
-                        {open ? (
-                            <Box mt={0.5}>
-                                <Divider />
-                                <Example />
-                                <Link onClick={() => setOpen(false)}>
-                                    &raquo; close
-                                </Link>
-                            </Box>
-                        ) : (
-                            <Link onClick={() => setOpen(true)}>
-                                &raquo; read more
-                            </Link>
-                        )}
-                    </Paper>
+                <Box my={2}>
+                    {posts.map((v) => (
+                        <PostCard key={v.path} meta={v} />
+                    ))}
                 </Box>
                 <Footer />
             </Box>
         </ThemeProvider>
+    );
+};
+
+const PostCard: React.VFC<{ meta: PostMeta }> = ({ meta }) => {
+    return (
+        <Box m={2}>
+            <Paper sx={{ px: 2, py: 0.5 }}>
+                <Typography variant="body2">{meta.published}</Typography>
+                <Typography variant="h6">{meta.title}</Typography>
+                <Link href={`/posts/${meta.path}`}>&raquo; read more</Link>
+            </Paper>
+        </Box>
     );
 };
